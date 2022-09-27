d96a311fd27f7d1ae6fa8c7583eb36e570a53d89
finnius25 <finneykoshy@gmail.com>
2022-09-27 17:24:02 -0500

    connect api data to variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import bg from "./assets/bg-img.jpg";
+import axois from 'axios'
 
 const App = () => {
+  const [weatherData, setWeatherData] = useState({})
   const [temp, setTemp] = useState();
-  const [location, setLocation] = useState();
+  const [location, setLocation] = useState('Dallas');
   const [weatherDescription, setWeatherDescription] = useState();
   const [isCelcius, setIsCelcius] = useState(false);
 
@@ -12,24 +14,34 @@ const App = () => {
     backgroundImage: `url(${bg})`,
   };
 
-  const APIKEY = "19764aafca74765d182a3e96f40348ec";
+  const API_KEY = "19764aafca74765d182a3e96f40348ec";
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=London&appid=${API_KEY}`
+
+  const fetchWeatherData = async () => {
+    const { data } = await axois.get(API_URL)
+
+    setWeatherData({
+      temp : data.main.temp,
+      city : data.name, 
+      country : data.sys.country,
+      description : data.weather[0].description
+    })
+  }
 
   useEffect(() => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=Dallas,TX&appid=${APIKEY}`
-    )
-      .then((res) => res.json())
-      .then((res) => console.log(res));
+    fetchWeatherData();
   }, []);
+
+
 
   return (
     <div className="App" style={styles}>
       <h1>
-        {temp}
+        {weatherData.temp}
         <span>{isCelcius ? "℃" : "℉"}</span>
       </h1>
-      <h2>{location}</h2>
-      <h4>{weatherDescription}</h4>
+      <h2>{weatherData.city}, {weatherData.country}</h2>
+      <h4>{weatherData.description}</h4>
     </div>
   );
 };
