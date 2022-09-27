efbf081a7d9f100bb392114e38b5255fd35e92fa
finnius25 <finneykoshy@gmail.com>
2022-09-27 16:42:38 -0500

    create variables & API call

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import './App.css';
-import bg from './assets/bg-img.jpg'
+import { useEffect, useState } from "react";
+import "./App.css";
+import bg from "./assets/bg-img.jpg";
 
-function App() {
+const App = () => {
+  const [temp, setTemp] = useState();
+  const [location, setLocation] = useState();
+  const [weatherDescription, setWeatherDescription] = useState();
+  const [isCelcius, setIsCelcius] = useState(false);
 
   const styles = {
-    backgroundImage : `url(${bg})`,
-    objectFit : "contain"
-  }
+    backgroundImage: `url(${bg})`,
+  };
+
+  const APIKEY = "19764aafca74765d182a3e96f40348ec";
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=Dallas,TX&appid=${APIKEY}`
+    )
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  }, []);
 
   return (
     <div className="App" style={styles}>
-      <h1>75℉</h1>
-      <h2>Dallas, Texas</h2>
-      <h4>clear, sunny</h4>
+      <h1>
+        {temp}
+        <span>{isCelcius ? "℃" : "℉"}</span>
+      </h1>
+      <h2>{location}</h2>
+      <h4>{weatherDescription}</h4>
     </div>
   );
-}
+};
 
 export default App;
